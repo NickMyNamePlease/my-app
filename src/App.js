4c149decf27d6cc1d3e1558d9b22234c24b5e06e
NickMyNamePlease <wertmanick1997@gmail.com>
2023-04-05 23:48:33 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -11,20 +11,38 @@ class App extends Component
     // .then(response => console.log(response));
     .then(response => this.setState({id: response.data.id}))
   }
+
+  handleClick2(){
+    // console.log('done!');
+    axios.get('https://api.github.com/users/maecapozzi')
+    // .then(response => console.log(response));
+    .then(response => this.setState({node_id: response.data.node_id}))
+  }
+
   constructor(){
     super();
     this.state = {
       id: ''
     }
     this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      node_id: ''
+    }
+    this.handleClick2 = this.handleClick2.bind(this);
+
   }
 
   render()
   {
     return (<div className='button_container'>
-    <button className='button' onClick={this.handleClick}>push me</button>
+    <button className='button1' onClick={this.handleClick}>push me</button>
     <p>{this.state.id}</p>
+    
+    <button className='button2' onClick={this.handleClick2}>push me</button>
+    <p>{this.state.node_id}</p>
     </div>)
+    
   }
 }
 export default App;
