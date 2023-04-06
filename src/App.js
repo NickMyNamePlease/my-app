64aeec4766eb122c4da7f25e056f0aceef763a72
NickMyNamePlease <wertmanick1997@gmail.com>
2023-04-06 11:00:39 +0300

    modified:   src/App.css modified:   src/App.js modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import './App.css';
+import './index.css';
 import axios from 'axios';
 
 
 class App extends Component
 {
+  constructor(){
+    super();
+    this.state = {
+      id: '',
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   handleClick(){
     // console.log('done!');
     axios.get('https://api.github.com/users/maecapozzi')
@@ -12,35 +21,14 @@ class App extends Component
     .then(response => this.setState({id: response.data.id}))
   }
 
-  handleClick2(){
-    // console.log('done!');
-    axios.get('https://api.github.com/users/maecapozzi')
-    // .then(response => console.log(response));
-    .then(response => this.setState({node_id: response.data.node_id}))
-  }
-
-  constructor(){
-    super();
-    this.state = {
-      id: ''
-    }
-    this.handleClick = this.handleClick.bind(this);
-
-    this.state = {
-      node_id: ''
-    }
-    this.handleClick2 = this.handleClick2.bind(this);
-
-  }
 
   render()
   {
-    return (<div className='button_container'>
-    <button className='button1' onClick={this.handleClick}>push me</button>
-    <p>{this.state.id}</p>
     
-    <button className='button2' onClick={this.handleClick2}>push me</button>
-    <p>{this.state.node_id}</p>
+
+    return (<div className='button_container'>
+    <button className='button' onClick={this.handleClick}>push me</button>
+    <p>{this.state.id}</p>
     </div>)
     
   }
