978691a1ffc52dae93fb0f0a7dedf817cad78eed
NickMyNamePlease <wertmanick1997@gmail.com>
2023-04-06 18:46:26 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -10,27 +10,36 @@ class App extends Component
     super();
     this.state = {
       id: '',
+      colors: ["red", "yellow", "blue", "green", "purple", "pink"]
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
+  restartButton(event) {
+    this.setState({ id: [] });
+  }
+
   handleClick(){
-    // console.log('done!');
     axios.get('https://api.github.com/users/maecapozzi')
-    // .then(response => console.log(response));
     .then(response => this.setState({id: response.data.id}))
   }
 
+  changeBg() {
+    const { colors } = this.state;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = color;
+  }
 
   render()
   {
-    
-
     return (<div className='button_container'>
-    <button className='button' onClick={this.handleClick}>push me</button>
-    <p>{this.state.id}</p>
-    </div>)
-    
+      <h4><button className='button1' onClick={this.handleClick}>Push Me</button>
+    <p>{this.state.id}</p></h4>
+    <h4><button className = "button2" onClick={() => this.changeBg()}>Change Color</button></h4>
+    <h4><button className ="button3" onClick={ this.restartButton.bind(this) }>Reload</button></h4>
+    </div>) 
   }
+  
+  
 }
 export default App;
